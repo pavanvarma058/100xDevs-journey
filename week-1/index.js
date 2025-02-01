10e4f3f18b38aa1764bccac9b9620b7e8b33da5d
Pavan Varma <pavanvarma058@gmail.com>
2025-02-01 22:33:50 +0530

    started learning JS foundations from week-1

---
+++ week-1/index.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+console.log(a); // here the variable 'a' is not defined but the code will run the first line and when comes to 2nd line it shoes error
+// this is how javascript works as a interpreted language
+// where as in compiled languages like C++, Java it won't work like this way they directly throws the error
