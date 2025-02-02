0fc3e7434934f5061ff6d722c2a659ff08e0ee75
Pavan Varma <pavanvarma058@gmail.com>
2025-02-02 05:56:34 +0530

    learned about data-types and variables in javascript from week-1

---
+++ week-1/data-types.js
@@ -0,0 +1,5 @@
+let myName = "Pavan";
+let age = 24;
+let isMarried = false;
+
+console.log("this person name is " + myName + " and their age is " + age);
---
+++ week-1/variables.js
@@ -0,0 +1,10 @@
+var a = 1; // var is a variable to define the value and it is used when ever we don't want to change the value
+a = 2;
+console.log(a); // 2
+
+let b = 1; // let is also variable to define the value and it is used when ever we want to change the value
+b = 3;
+console.log(b); // 3
+
+const c = 2; // it means constant, we cannot change the value once it is defined as const, if we try to change it throws error
+console.log(c);
