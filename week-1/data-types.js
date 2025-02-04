8d99e20eff47531dc0e038a020bbfe231491cdfe
Pavan Varma <pavanvarma058@gmail.com>
2025-02-04 20:59:58 +0530

    learned about data-types conditional statements and loops in JS

--- week-1/data-types.js
+++ week-1/data-types.js
@@ -1,5 +1,22 @@
-let myName = "Pavan";
-let age = 24;
-let isMarried = false;
+let myName = "Pavan"; // string
+let age = 24; // int
+let isMarried = false; // boolean
 
 console.log("this person name is " + myName + " and their age is " + age);
+
+// conditional statements
+if (isMarried == true) {
+  console.log(myName + " is married");
+} else {
+  console.log(myName + " is not married");
+}
+
+// Write a program that greets a person based on their gender. (If else)
+
+let gender = "female";
+
+if (gender == "male") {
+  console.log("Hi sir hope you're doing well..!");
+} else if (gender == "female") {
+  console.log("Hi mam hope you're doing well..!");
+}
---
+++ week-1/loops.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+// for loop
+// for (let i = 0; i <= 100; i++) {
+//   count = i;
+//   console.log(i);
+// }
+
+// while loop
+while (count <= 100) {
+  console.log(count++);
+}
