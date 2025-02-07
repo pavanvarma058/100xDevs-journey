a652404a58090e6067109eca8de06083eb786329
Pavan Varma <pavanvarma058@gmail.com>
2025-02-07 23:08:54 +0530

    added a new function to implement it on setInterval

--- week-1/advFunctions.js
+++ week-1/advFunctions.js
@@ -8,5 +8,8 @@ setTimeout(greet, 3 * 1000);
 
 // set interval function
 
+function repeat() {
+  console.log("I'm repeating the same thing again and again until you stop..!");
+}
 // this function is used to call the function continously after defining some interval time
-setInterval(greet, 2 * 1000);
+setInterval(repeat, 2 * 1000);
