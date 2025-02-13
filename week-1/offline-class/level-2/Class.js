d3bac5641b1e01eaa1792de987bf7e5edb9344d1
Pavan Varma <pavanvarma058@gmail.com>
2025-02-13 18:16:10 +0530

    Learned about Class in JS

---
+++ week-1/offline-class/level-2/Class.js
@@ -0,0 +1,20 @@
+class Animal {
+  constructor(name, legCount, speaks) {
+    this.name = name;
+    this.legCount = legCount;
+    this.speaks = speaks;
+  }
+
+  static myType() {
+    console.log("Animal");
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs and it ${this.speaks}`;
+  }
+}
+
+console.log(Animal.myType());
+let dog = new Animal("Dog", 4, "bhow bhow"); // create an object
+console.log(dog.describe()); // call function on object
+
+// classes means give out a structure of something that is resuable in multiple places
