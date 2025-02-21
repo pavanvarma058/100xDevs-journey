e8b2eb2a004207804316661f9acacf26d41a1c71
Pavan Varma <pavanvarma058@gmail.com>
2025-02-21 15:06:12 +0530

    learned about body-parser

--- week-2/2.2/http-server-2/index.js
+++ week-2/2.2/http-server-2/index.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/route-handler", (req, res) => {
-  res.json({
-    name: "Pavan Varma",
-    age: 24,
-  });
-});
+// middlewares
+app.use(bodyParser.json());
+// app.get("/route-handler", (req, res) => {
+//   // header, body , query parameters
+//   // do machine learning model
+//   res.json({
+//     name: "Pavan Varma",
+//     age: 24,
+//   });
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hi if you want to know my name and age go to /route-handler");
+// app.get("/", (req, res) => {
+//   res.send("Hi if you want to know my name and age go to /route-handler");
+// });
+
+app.post("/", (req, res) => {
+  // to get the body req from the user in post request we have to use body-parser
+  // bcoz by default express does not parse the body of the request
+  // we have to use body-parser to parse the body of the request
+  console.log(req.body);
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
